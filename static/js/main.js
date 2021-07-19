208d1318d771a307534f476088d7abce13d0b3e9
Federico Pomar <fmpomar@users.noreply.github.com>
2021-07-19 16:58:17 -0300

    First commit

---
+++ static/js/main.js
@@ -0,0 +1,85 @@
+import init, { AsyncGameEngine } from '/pkg/ao_rust_web.js';
+await init();
+// await run();
+// await gl_main();
+
+let canvas = document.getElementById("canvas");
+let gameEngine = await AsyncGameEngine.new("/ao-wasm-site/static/asset/", canvas);
+let cameraSpeed = {x: 0.0, y: 0.0};
+let mapIndex = 1;
+
+await gameEngine.set_map(mapIndex);
+
+let last;
+function step(timestamp) {
+    if (last === undefined) {
+        last = timestamp;
+    }
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    gameEngine.step((timestamp - last) / 1000.0);
+    last = timestamp;
+    window.requestAnimationFrame(step);
+}
+
+window.requestAnimationFrame(step);
+
+function updateCameraSpeed() {
+    let norm = Math.sqrt(cameraSpeed.x*cameraSpeed.x + cameraSpeed.y*cameraSpeed.y);
+    if (norm > 0.0) {
+        cameraSpeed.x /= norm;
+        cameraSpeed.y /= norm;
+    }
+    gameEngine.set_camera_speed(cameraSpeed.x, cameraSpeed.y);
+}
+
+document.addEventListener('keypress', async function (e) {
+    switch(e.key) {
+        case 'n':
+            mapIndex++;
+            break;
+        case 'p':
+            mapIndex--;
+            break;
+    }
+    if (mapIndex < 1) mapIndex = 1;
+    console.log("Loading map ", mapIndex);
+    await gameEngine.set_map(mapIndex);
+    // map = (await gameEngine.fetch_map('' mapIndex));
+})
+
+document.addEventListener('keydown', function (e) {
+    switch (e.key) {
+        case 'ArrowDown':
+            cameraSpeed.y = 1.0;
+            break;
+        case 'ArrowUp':
+            cameraSpeed.y = -1.0;
+            break;
+        case 'ArrowRight':
+            cameraSpeed.x = 1.0;
+            break;
+        case 'ArrowLeft':
+            cameraSpeed.x = -1.0;
+            break;
+    }
+    updateCameraSpeed();
+});
+
+document.addEventListener('keyup', function (e) {
+    switch (e.key) {
+        case 'ArrowDown':
+            cameraSpeed.y = 0.0;
+            break;
+        case 'ArrowUp':
+            cameraSpeed.y = 0.0;
+            break;
+        case 'ArrowRight':
+            cameraSpeed.x = 0.0;
+            break;
+        case 'ArrowLeft':
+            cameraSpeed.x = 0.0;
+            break;
+    }
+    updateCameraSpeed();
+});
